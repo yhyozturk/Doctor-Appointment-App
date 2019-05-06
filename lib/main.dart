3ad1ae8cc0f087477c580b552aa3ba770955f29d
yhyozturk <yahyaa.ozturrk@gmail.com>
2019-05-06 12:10:56 +0300

    try fork sync v2

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fast-Anadolu Project',
+      title: 'Fast-Anadolu',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
